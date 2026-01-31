c4b2171ce389fa1ef4a462572ac04122b7ef6267
Roma <roma@Daryas-MacBook-Pro.local>
2026-01-31 19:24:21 +0300

    first commit

--- js-files/modals.js
+++ js-files/modals.js
@@ -639,11 +639,12 @@ function initModalSwipe() {
 
 function showModal(product) {
   renderProductModal(product);
+  modal.classList.remove('hidden');
+  document.body.style.overflow = 'hidden';
+
   // при любом новом открытии — скролл модалки с нуля
   const scrollContainer = document.querySelector('#modalContent .flex-1');
   if (scrollContainer) scrollContainer.scrollTop = 0;
-  modal.classList.remove('hidden');
-  document.body.style.overflow = 'hidden';
 
   tg?.expand();
 }
